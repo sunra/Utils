d417151abbbc0802e78ad0463c7ea8537b1d3d4c
sunra <sunra@yandex.ru>
2015-09-01 17:19:42 +0300

    moved files for psr-0 compartibility

--- src/Sunra/Utils/Text/Keywords.php
+++ src/Sunra/Utils/Text/Keywords.php
@@ -2,12 +2,14 @@
 
     namespace Sunra\Utils\Text;
 
+    use \Sunra\HtmlToText;
+
     class Keywords
     {
         static public function getKeywords($text, $defaultKeywords = '')
         {
             $wordCounter = new WordCounter();
-
+//\Sunra\HtmlToText\HtmlToText::/*plain_text*/filter( $topic_text, 'br,b,p,i' );
             $keywords = $wordCounter->get_keywords($text, $defaultKeywords);
 
             return $keywords;
